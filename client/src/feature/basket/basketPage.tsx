29bc3a4d60a51c941d7733ae9425e1e7cd7116c9
Suresh Basappa <sureshdug@gmail.com>
2023-01-08 12:49:15 +0300

    End of Trail

--- client/src/feature/basket/basketPage.tsx
+++ client/src/feature/basket/basketPage.tsx
@@ -1,12 +1,10 @@
 import { Add, Delete, Remove } from "@mui/icons-material";
 import { LoadingButton } from "@mui/lab";
-import { Box, Button, Grid, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Box, Button, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import agent from "../../app/api/agent";
 import { useStoreContext } from "../../app/context/storeContext";
-import LoadingComponent from "../../app/layout/LoadingComponent";
-import { Basket } from "../../app/models/basket";
 import BasketSummary from "./basketSummary";
 
 export default function basketPage() {
